bf0a35c7ec2e17811ec916e68b496f3080381dac
kendra-svg <ksanchov@ucenfotec.ac.cr>
2025-01-28 20:25:59 -0600

    Add project files.

---
+++ furtytechcr-sln/src/app/app.test/app.test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  standalone: false,
+  templateUrl: './app.test.component.html',
+  styleUrl: './app.test.component.css'
+})
+export class AppTestComponent {
+  title = 'this is a test';
+
+}
